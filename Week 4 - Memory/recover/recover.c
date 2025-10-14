7fcfd818b078399af7300c1604b1f83d9187fb42
emmatey <mcrib40@gmail.com>
2025-10-14 00:11:11 -0400

    Added pset 4.3 'recover'.

---
+++ Week 4 - Memory/recover/recover.c
@@ -0,0 +1,66 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Read the file with fread to get the pointer to the start of the file.
+    if (argc != 2)
+    {
+        printf("Please provide only one argument, a file path to the input.\n");
+        return 1;
+    }
+    FILE *mem_card = fopen(argv[1], "r");
+    if (mem_card == NULL)
+    {
+        printf("Input file %s could not be read.\n", argv[1]);
+        fclose(mem_card);
+        return 1;
+    }
+
+    unsigned char *read_buffer = malloc(sizeof(unsigned char) * 512);
+    if (read_buffer == NULL)
+    {
+        free(read_buffer);
+        printf("unable to allocate memory for read_buffer.\n");
+        return 2;
+    }
+    // initialize to -1 to catch picture 000
+    int counter = -1;
+    FILE *nth_file = NULL;
+    bool found = false;
+
+    while (fread(read_buffer, 512, 1, mem_card) == 1)
+    {
+        bool jpg_sig = (read_buffer[0] == 0xff) && (read_buffer[1] == 0xd8) &&
+                       (read_buffer[2] == 0xff) && (read_buffer[3] >= 0xe0) &&
+                       (read_buffer[3] <= 0xef);
+
+        if (jpg_sig)
+        {
+            counter++;
+            found = true;
+
+            if (nth_file != NULL)
+            {
+                fclose(nth_file);
+            }
+
+            char new_file_name[8];
+            sprintf(new_file_name, "%.3i.jpg", counter);
+
+            nth_file = fopen(new_file_name, "w");
+            if (nth_file != NULL)
+            {
+                fwrite(read_buffer, 512, 1, nth_file);
+            }
+        }
+        else if (found == true)
+        {
+            fwrite(read_buffer, 512, 1, nth_file);
+        }
+    }
+    fclose(mem_card);
+    fclose(nth_file);
+    free(read_buffer);
+}
